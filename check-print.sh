c9513d58ae18974764b4d49cfeb2e1c8d2ef1c7a
Lukas Klein <lukas@lukasklein.com>
2022-10-07 09:19:24 +0200

    ✨ initial commit

---
+++ check-print.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+print_regex='[[:space:]]*print[[:space:](]*'
+did_find=0
+for fname in "$@"
+do
+    ffound=`grep -e ${print_regex} ${fname} | grep -e '\w' | wc -l`
+    if [ $ffound -gt 0 ]
+    then
+        echo "Found $ffound print statements in $fname"
+        did_find=1
+    fi
+done
+
+if [ $did_find -eq 0 ]
+then
+    echo "No print statements found"
+else
+    exit 1
+fi
